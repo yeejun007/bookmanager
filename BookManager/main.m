7b4f8f6e300ef9f4b6ad0ab9fca00c92261abaf9
yeejun007 <dlwns007@gmail.com>
2020-07-12 20:41:26 +0900

    Initial Commit

---
+++ BookManager/main.m
@@ -0,0 +1,17 @@
+//
+//  main.m
+//  BookManager
+//
+//  Created by 송이준 on 12/07/2020.
+//  Copyright © 2020 yeejun. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+int main(int argc, const char * argv[]) {
+    @autoreleasepool {
+        // insert code here...
+        NSLog(@"Hello, World!");
+    }
+    return 0;
+}
